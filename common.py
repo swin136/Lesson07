f21f90dbf79e81e0eae4c726fded1a917ad6670d
Dmitri Camenev <dmit.cameneff@yandex.ru>
2023-06-20 23:27:29 +0300

    Initial commit

---
+++ common.py
@@ -0,0 +1,125 @@
+import os
+import json
+
+DATA_DIR = 'data'
+STUDENTS_DATA = 'students.json'
+PROFESSIONS_DATA = 'professions.json'
+
+
+
+def load_students(filename: str):
+    """
+    Загружает список студентов из файла
+    :return: dict
+    """
+    try:
+        with open(os.path.join(DATA_DIR.strip(), filename.strip()), "r", encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+
+
+    except json.decoder.JSONDecodeError:
+        print(f'Ошибка декодирования данных студентов из файла {filename}')
+        return None
+
+    except FileNotFoundError:
+        print(f'Файл {filename} с данными студентов не найден.')
+        return None
+    except PermissionError:
+        print(f'Ошибка доступа к файлу {filename} с данными студентов.')
+        return None
+    except IOError:
+        print(f'Ошибка ввода-вывода при доступе к файлу {filename} с данными студентов.')
+        return None
+
+
+def load_professions(filename: str):
+    """
+    Загружает список профессий из файла
+    :return: dict
+    """
+    try:
+        with open(os.path.join(DATA_DIR.strip(), filename.strip()), "r", encoding='utf-8') as file:
+            data = json.load(file)
+        return data
+
+
+    except json.decoder.JSONDecodeError:
+        print(f'Ошибка декодирования данных профессий из файла {filename}')
+        return None
+
+    except FileNotFoundError:
+        print(f'Файл {filename} с данными профессий не найден.')
+        return None
+    except PermissionError:
+        print(f'Ошибка доступа к файлу {filename} с данными профессий .')
+        return None
+    except IOError:
+        print(f'Ошибка ввода-вывода при доступе к файлу {filename} с данными профессий.')
+        return None
+
+
+def get_student_by_pk(students: dict, pk: int):
+    """
+    Получает словарь с данными студента по его коду
+    :param students:
+    :param pk:
+    :return: dict (None - в случае ошибки)
+    """
+    try:
+        for student in students:
+            if student['pk'] == pk:
+                return student
+        return None
+    except KeyError:
+        print('Ошибка доступа к данным студентов!')
+        return None
+
+
+def get_profession_by_title(professions: list, title: str):
+    """
+    Получает словарь с данными профессии по её названию
+    :param professions:
+    :param title:
+    :return: dict (None - в случае ошибки)
+    """
+    try:
+        for profession in professions:
+            if profession['title'] == title:
+                return profession
+        return None
+    except KeyError:
+        print('Ошибка доступа к данным специальностям!')
+        return None
+
+def check_fitness(student, profession: dict):
+    """
+    Возращает словарь с параметрами пригодности студента к данной специальности
+    :param student:
+    :param profession:
+    :return: dict
+    """
+    try:
+        student_skills = set(student['skills'])
+        profession_skills = set(profession['skills'])
+
+        # print(f"Данные студента {student_skills}")
+        # print(f"Данные профессии {profession_skills}")
+
+        has = student_skills.intersection(profession_skills)
+        lacks = profession_skills.difference(student_skills)
+
+        # print(f"Знает {has}")
+        # print(f"Не знает {lacks}")
+
+        fit_percent = round(len(has) / len(profession_skills) * 100)
+        # print(f"Пригодность {fit_percent}%")
+        return {
+            'has': list(has),
+            'lacks': list(lacks),
+            'fit_percent': fit_percent
+        }
+
+    except KeyError:
+        return None
+
---
+++ lesson07.py
@@ -0,0 +1,60 @@
+from common import *
+
+def main():
+    """
+    Реализует бизнес-логику проекта
+    :return: None
+    """
+    # Инициализация исходных данных для работы программы
+
+    students = load_students(STUDENTS_DATA)
+    professions = load_professions(PROFESSIONS_DATA)
+
+    if students is None or professions is None:
+        print('Ошибка инициализации данных!\nПрограмма будет завершенна')
+        return None
+
+    # Получаем номер студента
+    while True:
+        user_choice = input('Введите номер студента ').strip()
+        if user_choice.isdigit():
+            break
+
+    # Получаем данные о студенте
+    student = get_student_by_pk(students, int(user_choice))
+
+    # Студента с таким номером нет
+    if student is None:
+        print("У нас нет такого студента")
+        return None
+    # Выод информации о студенте и его навыках
+    print(f"Студент {student['full_name']}")
+    print(f"Знает {', '.join(student['skills'])}")
+
+    user_choice = ""
+    while True:
+        user_choice = input(f'Выберите специальность для оценки студента {student["full_name"]} ').strip().title()
+        if len(user_choice) > 0:
+            break
+
+    profession = get_profession_by_title(professions, user_choice)
+    if profession is None:
+        print("У нас нет такой специальности")
+        return None
+    # Вычисляем пригодность студента к выбранной специальности
+    fitness = check_fitness(student, profession)
+    if fitness is None:
+        print('Критическая ошибка выполнения программы!')
+        return None
+    # Выводим результаты
+    print(f"Пригодность {fitness['fit_percent']}%")
+    if fitness['fit_percent'] > 0:
+        print(f"{student['full_name']} знает {', '.join(fitness['has'])}")
+    print(f"{student['full_name']} не знает {', '.join(fitness['lacks'])}")
+    # print(profession['skills'])
+
+    input("[+] Нажмите Enter для завершения работы программы ... ")
+
+
+if __name__ == "__main__":
+    main()
